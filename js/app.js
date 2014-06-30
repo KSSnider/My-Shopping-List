978f2ba7013790100dde865e940aadf04a9bf1eb
Kevin Snider <kevin_snider@Kevins-MacBook-Pro.local>
2014-06-29 22:31:39 -0400

    javascript

---
+++ js/app.js
@@ -0,0 +1,75 @@
+$(document).ready(function() {
+	console.log('Welcome to console');
+    var submissions = 0;
+    var checkMark = '<input class="check"></input>';
+	var xMark = '<button class="x-mark"></button>';
+	$('.icon').hide();
+	$('.icon').fadeIn(1000);
+	$('#title').hide();
+	$('#title').fadeIn(1000);
+	
+	$('#reset').click(function(){
+
+});
+   /*Allows an "Enter" keystroke to work as a submit button for input field*/
+    function getItem() {
+        $('#add').keydown(function (enter) {
+            if (enter.keyCode == 13) {
+                postItem(); d
+            },m dc,s
+        });
+    }
+
+    getItem();
+
+    /*Takes returned value from previous function and manipulates it*/
+    function postItem() {
+        var item = $('#add').val();
+        var work = '<p class="full-width no-strikethrough">'+ xMark + item + checkMark + '</p>';
+        $('#list-area').prepend(work).fadeIn();
+        $('#add').val('');
+        $('#list-area p:first-child')
+        .css('opacity', "0")
+        .css("margin-top", "-20px")
+        .animate(
+            { opacity: "1" },
+            { queue: true, duration: 'slow' }
+        )
+        .animate(
+            {marginTop: "0px"},
+            { queue: false, duration: 'slow' }
+
+        );
+      
+    }
+ $('.check').append();
+       $('.alt-check').append();
+       $('x-mark').append();
+       $('alt-x-mark').append();
+});
+/*Clears all items on list*/
+$(document).on("click", "#reset", function(){
+    $("#list-area").empty();
+    submissions = 0;
+});
+/*Allows user to delete items*/
+$(document).on("click", ".x-mark", function(){
+    $(this).closest('p').fadeOut(300);
+});
+
+/*Allows user to check off items*/
+$(document).on("click", ".check", function(){
+    if (!$(this).closest('p').hasClass('strikethrough')) {
+        console.log("I am going to add a strikethrough");
+        $(this).closest('p').addClass("strikethrough");
+        $(this).siblings('.x-mark').addClass("alt-x-mark");
+        $(this).addClass("alt-check");
+   }
+   else {
+        console.log("I am going to remove a strikethrough");
+        $(this).closest('p').removeClass("strikethrough");
+        $(this).siblings('.x-mark').removeClass("alt-x-mark");
+        $(this).removeClass("alt-check");
+   }
+});
+
